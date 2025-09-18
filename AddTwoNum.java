d15f4e68e6eb0439c557aa13f6af491ca8cfa5b7
Rohan Yadav <yadavrohan98040@gmail.com>
2025-09-18 22:55:54 +0530

    first commit

---
+++ AddTwoNum.java
@@ -0,0 +1,8 @@
+public class AddTwoNum {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println("The sum is: " + c);
+    }
+}
---
+++ EvenoddCheck.java
@@ -0,0 +1,12 @@
+public class EvenoddCheck {
+    public static void main(String[] args) {
+        int num = 7;
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is Even");
+        } else {
+            System.out.println(num + " is Odd");
+        }
+    }
+}
+
