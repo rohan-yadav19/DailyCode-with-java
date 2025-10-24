e457a223c2b3cc84e87b193db88894edaf080248
Rohan Yadav <yadavrohan98040@gmail.com>
2025-10-24 21:05:40 +0530

    add:program to count no. of vowels amd consonants in given strings

---
+++ CountVowelsConsonants.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CountVowelsConsonants {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        int vowelCount = 0;
+        int consonantCount = 0;
+       str=str.toLowerCase();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    vowelCount++;
+                } else {
+                    consonantCount++;
+                }
+            }
+        }
+
+        System.out.println("Number of vowels: " + vowelCount);
+        System.out.println("Number of consonants: " + consonantCount);
+    }
+}
