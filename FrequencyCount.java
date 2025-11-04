12ca3e41aa5ab730b39b295e6d96a9ae034d4ab3
Rohan Yadav <yadavrohan98040@gmail.com>
2025-11-05 01:56:10 +0530

    add:progarm to count frequency in the given array

---
+++ FrequencyCount.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class FrequencyCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input array size
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        // Input array elements
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        boolean[] visited = new boolean[n]; // To track counted elements
+
+        System.out.println("\nElement | Frequency");
+        System.out.println("--------------------");
+
+        // Count frequency using nested loops
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) continue;
+
+            int count = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                    visited[j] = true;
+                }
+            }
+
+            System.out.println(arr[i] + "        | " + count);
+        }
+
+        sc.close();
+    }
+}
